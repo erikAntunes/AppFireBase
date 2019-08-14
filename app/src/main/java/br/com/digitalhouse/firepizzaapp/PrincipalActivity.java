091b7d9e9ce2f705e7b6df905b83d074cb9a2ba4
Erik Antunes <erik.antunes@live.com>
2019-08-14 10:33:50 -0300

    persistindo dados

--- app/src/main/java/br/com/digitalhouse/firepizzaapp/PrincipalActivity.java
+++ app/src/main/java/br/com/digitalhouse/firepizzaapp/PrincipalActivity.java
@@ -1,29 +1,44 @@
 package br.com.digitalhouse.firepizzaapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
-
+import java.io.ByteArrayOutputStream;
 
 
 public class PrincipalActivity extends AppCompatActivity {
 
     private TextView nomeTextView;
-    private ImageView perfilImageView;
+    private CircleImageView perfilImageView;
     private Button trocarFotoButton;
+    private FirebaseUser user;
 
+
+    FirebaseStorage storage = FirebaseStorage.getInstance();
     static final int REQUEST_IMAGE_CAPTURE = 1;
+
 
 
     @Override
@@ -32,14 +47,13 @@ public class PrincipalActivity extends AppCompatActivity {
         setContentView(R.layout.activity_principal);
 
 
+
         nomeTextView = findViewById(R.id.nome_bem_vindo_id);
         perfilImageView = findViewById(R.id.perfil_imageView_id);
         trocarFotoButton = findViewById(R.id.trocar_foto_button);
 
 
-
-
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             String name = user.getDisplayName();
 
@@ -57,6 +71,27 @@ public class PrincipalActivity extends AppCompatActivity {
         });
 
 
+        baixarFoto();
+
+    }
+
+    private void baixarFoto() {
+
+        StorageReference reference  = storage.getReference("perfil/"+user.getUid());
+
+        reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+
+                Picasso.get().load(uri).into(perfilImageView);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
+
     }
 
     private void tirarFoto() {
@@ -67,11 +102,36 @@ public class PrincipalActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
+            Bundle extras = intent.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             perfilImageView.setImageBitmap(imageBitmap);
+
+            StorageReference reference = storage.getReference("perfil/"+user.getUid());
+
+
+
+            // Get the data from an ImageView as bytes
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] data = baos.toByteArray();
+
+            UploadTask uploadTask = reference.putBytes(data);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    Toast.makeText(PrincipalActivity.this,"Upload Falhou",Toast.LENGTH_LONG).show();
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(PrincipalActivity.this,"Upload Concluído",Toast.LENGTH_LONG).show();
+                }
+            });
+
+
         }
     }
 }
